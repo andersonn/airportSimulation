1c8b6d7a3b5c887743002a18ab6de76e0f60c794
andersonn <njanderson2013@gmail.com>
2016-04-08 16:02:31 -0500

    airport texture

--- airport.js
+++ airport.js
@@ -2,17 +2,30 @@
 
 //create the scene
 var scene = new THREE.Scene();
-var geometry = new THREE.BoxGeometry(20,0.1, 2);
-var image = new THREE.TextureLoader().load("RunwayLarge.jpg");
+var geometry = new THREE.BoxGeometry(40,0.1, 20);
+var image = new THREE.TextureLoader().load("airport.jpg");
 var material = new THREE.MeshLambertMaterial({map : image});
 var runway = new THREE.Mesh(geometry, material);
 
 scene.add(runway);
 
+var loader = new THREE.ColladaLoader();
+var model = new THREE.Object3D();
+
+loader.load('b737/model.dae', function(collada){
+    model.add(collada.scene);
+    model.scale.multiplyScalar(0.05);
+    model.translateY(.1);
+    model.translateX(2.4);
+    model.translateZ(1);
+    scene.add(model);
+});
+
+
 var schedule = new Schedule(5, 96);
-schedule.printArrivals();
-console.log("Departures");
-schedule.printDepartures();
+//schedule.printArrivals();
+//console.log("Departures");
+//schedule.printDepartures();
 //lighting
 scene.add(new THREE.AmbientLight(0xffffff));
 var light1 = new THREE.DirectionalLight(0xaaaaaa);
@@ -22,11 +35,12 @@ scene.add(light1);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-renderer.setClearColor(0xffffff);
+renderer.setClearColor(0x3bb9ff);
 //camera
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-camera.position.z = 3;
+camera.position.y = 30;
+//camera.position.z = 3;
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 //update the airplanes
 function render(){
