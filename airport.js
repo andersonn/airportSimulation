c2fdfe4762d2e8a3c119108eca2712e5db947250
andersonn <njanderson2013@gmail.com>
2016-04-09 18:34:33 -0500

    major changes

--- airport.js
+++ airport.js
@@ -7,36 +7,57 @@ var image = new THREE.TextureLoader().load("airport.jpg");
 var material = new THREE.MeshLambertMaterial({map : image});
 var runway = new THREE.Mesh(geometry, material);
 
-scene.add(runway);
+var geometry2 = new THREE.BoxGeometry(40, 0.1, 10);
+var image2 = new THREE.TextureLoader().load("asphalt.jpg");
+var material2 = new THREE.MeshLambertMaterial({map : image2});
+var terminalGround = new THREE.Mesh(geometry2, material2);
+terminalGround.translateZ(15);
 
+scene.add(runway);
+scene.add(terminalGround);
 var loader = new THREE.ColladaLoader();
 var model = new THREE.Object3D();
 
 loader.load('b737/model.dae', function(collada){
     model.add(collada.scene);
     model.scale.multiplyScalar(0.05);
-    model.translateY(.1);
-    model.translateX(2.4);
-    model.translateZ(1);
+    //model.translateY(.1);
+    //model.translateX(-10);
+    //model.translateZ(12);
     scene.add(model);
 });
 
 var terminal = new THREE.Object3D();
 loader.load('terminal/model.dae', function(collada){
     terminal.add(collada.scene);
-    terminal.scale.multiplyScalar(.04);
-    terminal.rotateY(Math.PI/2);
+    terminal.scale.multiplyScalar(.045);
     terminal.translateY(.15)
     var mod = new THREE.Object3D();
     mod.add(terminal);
-    mod.translateX(19);
-    mod.translateZ(12);
+    mod.translateX(-10);
+    mod.translateZ(20);
     scene.add(mod);
 });
 var schedule = new Schedule(5, 96);
-//schedule.printArrivals();
-//console.log("Departures");
-//schedule.printDepartures();
+
+//Gate 1
+var curve = new THREE.SplineCurve3([
+    new THREE.Vector3(-10, .1, 1.7),
+    new THREE.Vector3(-5.45, .1, 1.7),
+    new THREE.Vector3(-4.4,.1, .75),
+    new THREE.Vector3(-4.4, .1, -1.6),
+    new THREE.Vector3(-2.8, .1, -2.1),
+    new THREE.Vector3(2.25, .1, -2.1),
+    new THREE.Vector3(3.1, .1, -1),
+    new THREE.Vector3(3.1, .1, 3),
+    new THREE.Vector3(2.25, .1, 3.5)
+]);
+var curveGeom = new THREE.Geometry();
+curveGeom.vertices = curve.getPoints(50);
+var curveMaterial = new THREE.LineBasicMaterial({color : 0xff0000});
+var splineObject = new THREE.Line(curveGeom, curveMaterial);
+scene.add(splineObject);
+console.log(curve.getPointAt(0));
 //lighting
 scene.add(new THREE.AmbientLight(0xffffff));
 var light1 = new THREE.DirectionalLight(0xaaaaaa);
@@ -54,8 +75,14 @@ camera.position.y = 30;
 //camera.position.z = 3;
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 //update the airplanes
+x=0;
 function render(){
     requestAnimationFrame(render);
     renderer.render(scene, camera);
+    if(x<1){
+        model.position.copy(curve.getPointAt(x));
+        model.lookAt(curve.getTangentAt(x).normalize());
+    }
+    x+=.01;
 };
 render();
